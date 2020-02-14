081c669e7c9f433a9fe7a500f307796080cda8bb
numanmunawar <34870451+numanmunawar@users.noreply.github.com>
2020-02-14 18:17:22 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,8 @@ import nav from '../components/nav';
 const About = () => (
   <Layout>
     <div>
-      <h1>About BitzPrice</h1>
-      <p>Application to view Bitcoin prices</p>
+      <h1>About CS Blog</h1>
+      <p>Application to view Blogs</p>
     </div>
   </Layout>
 );
